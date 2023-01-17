fc8fb3ce6ce313c5ee5e19bdb5cec74186c1b1f0
Mario Alviano <mario.alviano@gmail.com>
2023-01-17 19:35:57 +0100

    basic, abstract primitive domain classes

--- dumbo_utils/validation.py
+++ dumbo_utils/validation.py
@@ -17,3 +17,15 @@ def pattern(regex: str) -> Callable[[str], bool]:
 
     res.__name__ = f'pattern({regex})'
     return res
+
+
+@typeguard.typechecked
+def validate_does_not_have_attributes(cls, attributes: list[str], and_annotations: bool = False):
+    if and_annotations:
+        validate(
+            f"{cls.__name__} has no attribute __annotations__",
+            hasattr(cls, '__annotations__') and cls.__annotations__ != {},
+            equals=False
+        )
+    for attribute in attributes:
+        validate(f"{cls.__name__} has no attribute {attribute}", hasattr(cls, attribute), equals=False)
